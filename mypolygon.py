14681f90bd026b58cdd9774c629c2352b5bced12
kye8747 <43972604+kye8747@users.noreply.github.com>
2018-10-08 17:54:40 -0400

    Added python files for the course

---
+++ mypolygon.py
@@ -0,0 +1,12 @@
+import turtle
+bob = turtle.Turtle()
+
+def polygon(t, n, length):
+    angle = 360 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+polygon(bob, 7, 70)
+
+turtle.mainloop()
